607ee266faf68e5c28431b5484ebba2705ad3527
tauan-longaretti <tauanlongaretti@gmail.com>
2019-10-08 21:04:04 -0400

    Explained the 4 principles of this and completed code examples for principle 1 and 2

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. The global binding princle is that "this" will have a value of the window/console object if used in the global scope.
+* 2. In the inplicit binding principle, the value of this is determined by whichever object is left of the dot used to call a function.
+* 3. New binding principle means that this is determined by the specific instance of the object that is created and returned by a constructor function.
+* 4. The explicit binding principle overrides what the this keyword in bound to when using call, apply and bind methods.
 *
 * write out a code example of each explanation above
 */
@@ -12,14 +12,23 @@
 // Principle 1
 
 // code example for Window Binding
+console.log(this);
 
 // Principle 2
 
 // code example for Implicit Binding
+const myObj = {
+    warning: 'Beware of the',
+    speak: function(animal) {
+        console.log(`${this.warning} ${animal}!`);
+    }
+};
+myObj.speak('dog');
 
 // Principle 3
 
 // code example for New Binding
+
 
 // Principle 4
 
